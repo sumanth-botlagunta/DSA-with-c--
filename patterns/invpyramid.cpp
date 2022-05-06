2b77688a052945734be35a46678c82853841a5d7
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2022-05-06 15:07:02 +0530

    inverted half pyramid

---
+++ patterns/invpyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    n=7;
+    for(int i=7;i>0;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
